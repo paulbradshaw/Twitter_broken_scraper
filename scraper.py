9215709123c8f0063cecbb6ca3d194b17b67505a
Paul Bradshaw <paulonhismobile@gmail.com>
2016-01-19 07:25:17 +0000

    Pasted code from old broken Twitter scraper
    https://classic.scraperwiki.com/editor/raw/basic_twitter_scraper

--- scraper.py
+++ scraper.py
@@ -1,24 +1,28 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+###################################################################################
+# Twitter API scraper - designed to be forked and used for more interesting things
+###################################################################################
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+import scraperwiki
+import simplejson
+import urllib2
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+# Get results from the Twitter API! Change QUERY to your search term of choice. 
+# Examples: 'PCC "Police Commissioner"
+QUERY = 'Police Commissioner'
+RESULTS_PER_PAGE = '100'
+LANGUAGE = 'eng'
+NUM_PAGES = 20 
+
+for page in range(1, NUM_PAGES+1):
+    base_url = 'http://search.twitter.com/search.json?q=%s&rpp=%s' \
+         % (urllib2.quote(QUERY), RESULTS_PER_PAGE)
+    try:
+        results_json = simplejson.loads(scraperwiki.scrape(base_url))
+        for result in results_json['results']:
+            data = {}
+            data['id'] = result['id']
+            data['text'] = result['text']
+            data['from_user'] = result['from_user']
+            print data['from_user'], data['text']
+    except:
+        pass
